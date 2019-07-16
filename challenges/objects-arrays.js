8746e566b2d47bc22e89532e057c2ad5282f0ce3
timislearning <tim.mitzen@gmail.com>
2019-07-16 16:59:35 -0700

    more refactoring

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,11 +73,13 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-for(let i = 0; i < graduates.length; i++){
-  universities.push(graduates[i].university);
-  universities.sort();
-}
+const universities =
+// for(let i = 0; i < graduates.length; i++){
+//   universities.push(graduates[i].university);
+//   universities.sort();
+// }
+graduates.map(uni => uni.university);
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -86,6 +88,7 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
+console.log(universities)
 const contactInfo = [];
 graduates.forEach(university => contactInfo.push(`${university.first_name} ${university.email}`));
 
@@ -93,14 +96,18 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
-for(let i = 0; i < graduates.length; i++){
-  if(graduates[i].university.includes("Uni")){
-    uni.push(graduates[i]);
-  }
-}
-console.log(uni);
+const uni = 
+// for(let i = 0; i < graduates.length; i++){
+//   if(graduates[i].university.includes("Uni")){
+//     uni.push(graduates[i]);
+//   }
+// }
+graduates.map(university => university.university).filter(university => university.includes("Uni"));;
 
+
+
+console.log(uni);
+//graduates.forEach(university => uni.push(if(graduates[i].university.includes("Uni"))
 
 // ==== ADVANCED Array Methods ====
 
